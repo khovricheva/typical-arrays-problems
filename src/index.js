e99f98dd737a003f7f6c80c63d85f63a160a1e6f
khovricheva <kseniyap1102@gmail.com>
2020-09-30 17:57:23 +0300

    feat: add a solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        return Math.min(...array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        return Math.max(...array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        let sum = array.reduce(
+            (accumulator, currentValue) => accumulator + currentValue
+        );
+        return sum / array.length;
+    }
+    return 0;
+};
